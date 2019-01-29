23034c7c5f0ff3b096d5b1ac1362b70399263e11
PHILLIP ALEXANDER RISKIN <u1088799@umail.utah.edu>
2019-01-29 16:43:45 -0700

    SelectFunction now works properly

--- cs4150/PS3-5/PS3-5 Galaxy Quest/PS3-5 Galaxy Quest/Program.cs
+++ cs4150/PS3-5/PS3-5 Galaxy Quest/PS3-5 Galaxy Quest/Program.cs
@@ -5,31 +5,8 @@ namespace PS3_5_Galaxy_Quest
 {
     public class GalaxyQuester
     {
-        private bool IsInGalaxy(long[] a, long[] b, long dSquared)
-        {
-            long diffx, diffy, xSquared, ySquared;
-            // Calculate distance formula for the given elements using distance of dSquared.
-            diffx = (a[0] - b[0]);
-            diffy = (a[1] - b[1]);
-            xSquared = FastMult(diffx, diffx);
-            ySquared = FastMult(diffy, diffy);
-            if (xSquared + ySquared <= dSquared)
-                return true;
-            return false;
-        }
-
-        private int CountOccurencesStar(List<long[]> A, long[] b, long dSquared)
-        {
-            int ret = 0;
-            // for each element of list A, compare to element b and count true.
-            for (int i = 0; i < A.Count; i++)
-            {
-                if (IsInGalaxy(A[i], b, dSquared))
-                    ret++;
-            }
-            return ret;
-        }
-
+        #region backup
+        /*
         private long[] FindMajorityStar(List<long[]> A, long dSquared)
         {
             // Calculate if even or odd length list.
@@ -83,6 +60,69 @@ namespace PS3_5_Galaxy_Quest
                 }
             }
         }
+        */
+        #endregion
+        private bool IsInGalaxy(long[] a, long[] b, long dSquared)
+        {
+            long diffx, diffy, xSquared, ySquared;
+            // Calculate distance formula for the given elements using distance of dSquared.
+            diffx = (a[0] - b[0]);
+            diffy = (a[1] - b[1]);
+            xSquared = FastMult(diffx, diffx);
+            ySquared = FastMult(diffy, diffy);
+            if (xSquared + ySquared <= dSquared)
+                return true;
+            return false;
+        }
+
+        private int CountOccurencesStar(List<long[]> A, long[] b, long dSquared)
+        {
+            int ret = 0;
+            // for each element of list A, compare to element b and count true.
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (IsInGalaxy(A[i], b, dSquared))
+                    ret++;
+            }
+            return ret;
+        }
+
+        private long[] FindMajorityStar(List<long[]> A, long dSquared)
+        {
+            // If empty list, no majority.
+            if (A.Count == 0)
+                return new long[] { -1, -1 };
+            // If single element list, majority is that element.
+            if (A.Count == 1)
+                return A[0];
+            int half_len;
+            long[] champ = A[0];
+            int count = 0;
+            // Figure out len/2 based on odd or even length list.
+            if (A.Count % 2 == 1)
+                half_len = (A.Count - 1) / 2;
+            else
+                half_len = A.Count / 2;
+            // Iterate through list and try to find majority element.
+            for (int i = 0; i < A.Count; i += 2)
+            {
+                if (count == 0)
+                    champ = A[i];
+                if (IsInGalaxy(champ, A[i], dSquared))
+                    count++;
+                else
+                    count--;
+            }
+            // If have majority element count number of matches to make
+            // sure it really is majority element and return it if so.
+            if (count > 0)
+            {
+                if (CountOccurencesStar(A, champ, dSquared) > half_len)
+                    return champ;
+            }
+            // No majority element found, return false.
+            return new long[] { -1, -1 };
+        }
 
         private long FastMult(long a, long b)
         {
@@ -90,7 +130,7 @@ namespace PS3_5_Galaxy_Quest
             return a * b;
         }
 
-        public long CalculateGalaxies(List<long[]> stars, int d, int k)
+        public int CalculateGalaxies(List<long[]> stars, int d, int k)
         {
             // Set ret for false.
             int ret = -1;
@@ -104,23 +144,18 @@ namespace PS3_5_Galaxy_Quest
             {
                 ret = 0;
                 // Count all stars near given star. This will count the given star as well.
-                for (int i = 0; i < stars.Count; i++)
-                {
-                    if (IsInGalaxy(the_star, stars[i], dSquared))
-                        ret++;
-                }
+                ret = CountOccurencesStar(stars, the_star, d * d);
             }
             // return result.
-            if (ret > 0)
-                return ret;
-            else
-                return -1;
+            return ret;
         }
 
         public GalaxyQuester() { }
 
         public static void Main(string[] args)
         {
+            Console.WriteLine(21/2);
+            /*
             string usr_input;
             string[] temp_nums;
             int[] parameters;
@@ -152,6 +187,7 @@ namespace PS3_5_Galaxy_Quest
             else
                 Console.WriteLine("NO");
             Console.Read();
+            */
         }
     }
 }
