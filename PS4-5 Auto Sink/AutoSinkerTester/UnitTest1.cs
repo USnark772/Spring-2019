fc57c1434497ef0d11c691cd64af465928a3d0e4
Phillip Riskin <phillipriskin@gmail.com>
2019-02-02 20:49:01 -0700

    Implemented untested DFS algorithm

--- PS4-5 Auto Sink/AutoSinkerTester/UnitTest1.cs
+++ PS4-5 Auto Sink/AutoSinkerTester/UnitTest1.cs
@@ -12,49 +12,58 @@ namespace AutoSinkerTester
         [TestMethod]
         public void KattisPublicTest1()
         {
-            Dictionary<string, int> tolls = new Dictionary<string, int>();
+            string[] cities = new string[] { "Sourceville", "SinkCity", "Easton", "Weston" };
+            int[] prices = new int[] { 5, 10, 20, 15 };
+            Dictionary<string, Vertex> graph = new Dictionary<string, Vertex>();
             Tuple<string, string>[] trips = new Tuple<string, string>[6];
-            Dictionary<string, List<string>> highways = new Dictionary<string, List<string>>();
-            int[] answers = new int[] { 25, 10, 0, 0};
-            tolls.Add("Sourceville", 5);
-            tolls.Add("SinkCity", 10);
-            tolls.Add("Easton", 20);
-            tolls.Add("Weston", 15);
-            highways.Add("Sourceville", new List<string>() { "Easton" });
-            highways["Sourceville"].Add("Weston");
-            highways.Add("Weston", new List<string>() { "SinkCity" });
-            highways.Add("Easton", new List<string>() { "SinkCity" });
-            trips[0] = new Tuple<string, string>("Sourceville", "SinkCity");
-            trips[1] = new Tuple<string, string>("Easton", "SinkCity");
-            trips[2] = new Tuple<string, string>("SinkCity", "SinkCity");
-            trips[3] = new Tuple<string, string>("Weston", "Weston");
-            trips[4] = new Tuple<string, string>("Weston", "Sourceville");
-            trips[5] = new Tuple<string, string>("SinkCity", "Sourceville");
+            int[] answers = new int[] { 25, 10, 0, 0 };
+            for (int i = 0; i < cities.Length; i++)
+            {
+                graph.Add(cities[i], new Vertex());
+                graph[cities[i]].price = prices[i];
+            }
+
+            graph[cities[0]].edges.Add(cities[2]);
+            graph[cities[0]].edges.Add(cities[3]);
+            graph[cities[3]].edges.Add(cities[1]);
+            graph[cities[2]].edges.Add(cities[1]);
+            trips[0] = new Tuple<string, string>(cities[0], cities[1]);
+            trips[1] = new Tuple<string, string>(cities[2], cities[1]);
+            trips[2] = new Tuple<string, string>(cities[1], cities[1]);
+            trips[3] = new Tuple<string, string>(cities[3], cities[3]);
+            trips[4] = new Tuple<string, string>(cities[3], cities[0]);
+            trips[5] = new Tuple<string, string>(cities[1], cities[0]);
             AutoSinker AS = new AutoSinker();
+            AS.PrepGraph(graph);
             for (int i = 0; i < 4; i++)
             {
-                Assert.AreEqual(answers[i], AS.CalculatePriceOfTrip(tolls, highways, trips[i]));
+                Assert.AreEqual(answers[i], AS.CalculatePriceOfTrip(graph, trips[i]));
             }
-            for(int j = 4; j < 6; j++)
+            for (int j = 4; j < 6; j++)
             {
-                Assert.AreEqual("NO", AS.CalculatePriceOfTrip(tolls, highways, trips[j]));
+                Assert.AreEqual("NO", AS.CalculatePriceOfTrip(graph, trips[j]));
             }
         }
 
         [TestMethod]
         public void KattisPublicTest2()
         {
-            Dictionary<string, int> tolls = new Dictionary<string, int>();
+            string[] cities = new string[] { "Here", "There" };
+            int[] prices = new int[] { 10, 20 };
+            Dictionary<string, Vertex> graph = new Dictionary<string, Vertex>();
             Tuple<string, string>[] trips = new Tuple<string, string>[6];
-            Dictionary<string, List<string>> highways = new Dictionary<string, List<string>>();
             int[] answers = new int[] { 25, 10, 0, 0 };
-            tolls.Add("Here", 10);
-            tolls.Add("There", 20);
-            trips[0] = new Tuple<string, string>("Here", "There");
-            trips[1] = new Tuple<string, string>("There", "There");
+            for (int i = 0; i < cities.Length; i++)
+            {
+                graph.Add(cities[i], new Vertex());
+                graph[cities[i]].price = prices[i];
+            }
+            trips[0] = new Tuple<string, string>(cities[0], cities[1]);
+            trips[1] = new Tuple<string, string>(cities[1], cities[1]);
             AutoSinker AS = new AutoSinker();
-            Assert.AreEqual("NO", AS.CalculatePriceOfTrip(tolls, highways, trips[0]));
-            Assert.AreEqual(0, AS.CalculatePriceOfTrip(tolls, highways, trips[1]));
+            AS.PrepGraph(graph);
+            Assert.AreEqual("NO", AS.CalculatePriceOfTrip(graph, trips[0]));
+            Assert.AreEqual(0, AS.CalculatePriceOfTrip(graph, trips[1]));
         }
     }
 }
--- PS4-5 Auto Sink/PS4-5 Auto Sink/Program.cs
+++ PS4-5 Auto Sink/PS4-5 Auto Sink/Program.cs
@@ -1,48 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PS4_5_Auto_Sink
 {
+    public class Vertex
+    {
+        public List<string> edges = new List<string>();
+        public int price = 0;
+        public int pre_value = 0;
+        public int post_value = 0;
+        public int top_sort_value = 0;
+        public Vertex() { }
+    }
+
+
+    /// <summary>
+    /// Manipulates a DAG for the purpose of calculating best routes between two places.
+    /// </summary>
     public class AutoSinker
     {
-        private Tuple<int, string>[] TopologicalSort(Dictionary<string, int> tolls, Dictionary<string, List<string>> highways)
-        {
-            Tuple<int, string>[] ret = new Tuple<int, string>[tolls.Count];
-
-            return ret;
-        }
-
-
-        private void DepthFirstSearch(Dictionary<string, List<string>> graph, ref Dictionary<string, int[]> pre_post_vals)
+        private void DepthFirstSearch(Dictionary<string, Vertex> graph)
         {
             int clock = 1;
-            Dictionary<string, bool> visited = new Dictionary<string, bool>();
-            foreach (var item in graph)
+            foreach (KeyValuePair<string, Vertex> vertex in graph)
             {
-                visited.Add(item.Key, false);
+                if (graph[vertex.Key].pre_value == 0)
+                    Explore(graph, vertex.Key, ref clock);
             }
-            foreach (var item in graph)
+        }
+
+        private void Explore(Dictionary<string, Vertex> graph, string v, ref int clock)
+        {
+            graph[v].pre_value = clock++;
+            foreach (string vertex in graph[v].edges)
             {
-                if (!visited[item.Key] == true)
-                    Explore(graph, item, ref pre_post_vals, clock);
+                if (graph[vertex].pre_value == 0)
+                    Explore(graph, vertex, ref clock);
             }
+            graph[v].post_value = clock++;
 
         }
 
-
-        private void Explore(Dictionary<string, List<string>> graph, KeyValuePair<string, List<string>> node, ref Dictionary<string, int[]> pre_post_vals, int clock)
+        /// <summary>
+        /// Use DFS to set pre and post values of given graph.
+        /// </summary>
+        /// <param name="graph"></param>
+        public void PrepGraph(Dictionary<string, Vertex> graph)
         {
-            
+            DepthFirstSearch(graph);
         }
-
-
-        public int CalculatePriceOfTrip(Dictionary<string, int> tolls, Dictionary<string, List<string>> highways, Tuple<string, string> trip)
+        public int CalculatePriceOfTrip(Dictionary<string, Vertex> graph, Tuple<string, string> trip)
         {
-            Dictionary<string, int[]> pre_post_vals = new Dictionary<string, int[]>();
-            DepthFirstSearch(highways, ref pre_post_vals);
+
             return -1;
         }
     }
@@ -55,9 +64,8 @@ namespace PS4_5_Auto_Sink
             string usr_input;
             int parameter, result = -1;
             string[] temp_usr_input;
-            Dictionary<string, int> tolls = new Dictionary<string, int>();
+            Dictionary<string, Vertex> graph = new Dictionary<string, Vertex>();
             Tuple<string, string>[] trips = new Tuple<string, string>[0];
-            Dictionary<string, List<string>> highways = new Dictionary<string, List<string>>();
             AutoSinker AS = new AutoSinker();
             // Get input
             for (int j = 0; j < 3; j++)
@@ -71,7 +79,8 @@ namespace PS4_5_Auto_Sink
                     {
                         usr_input = Console.ReadLine();
                         temp_usr_input = usr_input.Split(' ');
-                        tolls.Add(temp_usr_input[0], int.Parse(temp_usr_input[1]));
+                        graph.Add(temp_usr_input[0], new Vertex());
+                        graph[temp_usr_input[0]].price = int.Parse(temp_usr_input[1]);
                     }
                 }
                 else if (j == 1)
@@ -81,7 +90,7 @@ namespace PS4_5_Auto_Sink
                     {
                         usr_input = Console.ReadLine();
                         temp_usr_input = usr_input.Split(' ');
-                        highways[temp_usr_input[0]].Add(temp_usr_input[1]);
+                        graph[temp_usr_input[0]].edges.Add(temp_usr_input[1]);
                     }
                 }
                 else
@@ -97,9 +106,10 @@ namespace PS4_5_Auto_Sink
                     }
                 }
             }
-            foreach (var item in trips)
+            AS.PrepGraph(graph);
+            foreach (Tuple<string, string> trip in trips)
             {
-                result = AS.CalculatePriceOfTrip(tolls, highways, item);
+                result = AS.CalculatePriceOfTrip(graph, trip);
                 if (result > -1)
                     Console.WriteLine(result);
                 else
