26c116ee96ef10d9a323acc28a069659747798c2
Phillip Riskin <phillipriskin@gmail.com>
2019-03-19 20:15:08 -0600

    Started Bank Kattis solution. Have input capture working.

---
+++ PS8-6-Bank/PS8-6-Bank/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS8_6_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usr_input;
+            string[] temp_usr_input;
+            int N, T;
+            int[] a, b;
+            usr_input = Console.ReadLine();
+            temp_usr_input = usr_input.Split(' ');
+            int.TryParse(temp_usr_input[0], out N);
+            int.TryParse(temp_usr_input[1], out T);
+            a = new int[N];
+            b = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                usr_input = Console.ReadLine();
+                temp_usr_input = usr_input.Split(' ');
+                int.TryParse(temp_usr_input[0], out a[i]);
+                int.TryParse(temp_usr_input[1], out b[i]);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("a = " + a[i] + ", b = " + b[i]);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ PS8-6-Bank/PS8-6-Bank/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PS8-6-Bank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PS8-6-Bank")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5cb0c508-4f22-4af6-aea6-cb39d1d85eaf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
