aeb53f2f9626aec1287decab9ccfad0a65dbd669
Phillip Riskin <phillipriskin@gmail.com>
2019-04-03 20:32:05 -0600

    Filled out using info from PS10-2. Not done yet.

--- PS10-5-Narrow-Art-Gallery/PS10-5-Narrow-Art-Gallery/Program.cs
+++ PS10-5-Narrow-Art-Gallery/PS10-5-Narrow-Art-Gallery/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PS10_5_Narrow_Art_Gallery
 {
     public class GallerySolver
@@ -11,7 +12,7 @@ namespace PS10_5_Narrow_Art_Gallery
         public List<int> SolveGalleries()
         {
             List<int> ret = new List<int>();
-            int[] gallery;
+            int[,] gallery;
             string usr_input;
             string[] tmp_usr_input;
             usr_input = Console.ReadLine();
@@ -20,15 +21,15 @@ namespace PS10_5_Narrow_Art_Gallery
             int k = int.Parse(tmp_usr_input[1]);
             while (N != 0 && k != 0)
             {
-                gallery = new int[N*2+2];
-                gallery[0] = N;
-                gallery[1] = k;
-                for (int i = 2; i < N+2; i+=2)
+                gallery = new int[N + 2, 2];
+                gallery[0, 0] = N;
+                gallery[0, 1] = k;
+                for (int i = 2; i < N + 2; i += 2)
                 {
                     usr_input = Console.ReadLine();
                     tmp_usr_input = usr_input.Split(' ');
-                    gallery[i] = int.Parse(tmp_usr_input[0]);
-                    gallery[i+1] = int.Parse(tmp_usr_input[1]);
+                    gallery[i, 0] = int.Parse(tmp_usr_input[0]);
+                    gallery[i, 1] = int.Parse(tmp_usr_input[1]);
                 }
                 Console.WriteLine(SolveGallery(gallery));
                 usr_input = Console.ReadLine();
@@ -39,9 +40,47 @@ namespace PS10_5_Narrow_Art_Gallery
             return ret;
         }
 
-        public int SolveGallery(int[] gallery)
+        private int MaxVal(int r, int unclosable, int k, int[,] values)
         {
-            int ret=0;
+            int ret = 0;
+            int NminR = values[0, 0] - r;
+            if (k == NminR)
+            {
+                if (unclosable == 0)
+                    ret = values[r, 0] + MaxVal(r + 1, 0, k - 1, values);
+                else if (unclosable == 1)
+                    ret = values[r, 1] + MaxVal(r + 1, 1, k - 1, values);
+                else
+                    ret = Math.Max(
+                        values[r, 0] + MaxVal(r + 1, 0, k - 1, values),
+                        values[r, 1] + MaxVal(r + 1, 1, k - 1, values));
+            }
+            else if (k < NminR && k > 0)
+            {
+                if (unclosable == 0)
+                    ret = Math.Max(
+                        values[r, 0] + MaxVal(r + 1, 0, k - 1, values),
+                        values[r, 0] + MaxVal(r + 1, -1, k, values));
+                else if (unclosable == 1)
+                    ret = Math.Max(
+                        values[r, 1] + MaxVal(r + 1, 1, k - 1, values),
+                        values[r, 1] + MaxVal(r + 1, -1, k, values));
+                else
+                    ret = Math.Max(
+                        values[r, 0] + values[r, 1] + MaxVal(r + 1, 0, k - 1, values),
+                        Math.Max(
+                        values[r, 0] + values[r, 1] + MaxVal(r + 1, 1, k - 1, values),
+                        values[r, 0] + values[r, 1] + MaxVal(r + 1, -1, k, values)));
+            }
+            else if (k == 0)
+                ret = 0;
+            return ret;
+        }
+
+        public int SolveGallery(int[,] gallery)
+        {
+            int ret = 0;
+
             return ret;
         }
 
